74dce004efd5dc08321cd9b216d2298632d4d4cb
pooria2421 <pooria2421@gmail.com>
2026-04-30 16:09:40 +0330

    feat: validate video title in VideoCreateCommand constructor

--- src/modules/videos/commands/impl/video-create.command.ts
+++ src/modules/videos/commands/impl/video-create.command.ts
@@ -1,7 +1,10 @@
-import { CreateVideoInterface } from "../../domain/interfaces/create-video.interface";
+import { NotFoundException } from '@nestjs/common';
+import { CreateVideoInterface } from '../../domain/interfaces/create-video.interface';
 
 export class VideoCreateCommand {
-  constructor(
-    public readonly videoInput: CreateVideoInterface,
-  ) {}
+  constructor(public readonly videoInput: CreateVideoInterface) {
+    if (!videoInput.title) {
+      throw new NotFoundException('Title not found');
+    }
+  }
 }
